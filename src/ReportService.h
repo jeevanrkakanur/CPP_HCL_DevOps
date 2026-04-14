98631227dc9a0bd1ba09fe27f811b65ce4e2fda8
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-04-14 18:21:46 +0530

    adding another component

---
+++ src/ReportService.h
@@ -0,0 +1,14 @@
+#ifndef REPORTSERVICE_H
+#define REPORTSERVICE_H
+
+#include "MathApp.h"
+
+class ReportService {
+private:
+    MathApp app;
+
+public:
+    int generateReport(int a, int b);
+};
+
+#endif
